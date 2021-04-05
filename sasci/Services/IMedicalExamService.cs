f36547ab74af64ac9abb1686f81c50ac5b530327
davipviana <davipviana@gmail.com>
2021-04-05 10:28:28 -0300

    add complex model function

--- sasci/Services/IMedicalExamService.cs
+++ sasci/Services/IMedicalExamService.cs
@@ -10,19 +10,7 @@ namespace SASCi.Services
         [OperationContract]
         string Ping(string s);
 
-        /*[OperationContract]
+        [OperationContract]
         MedicalExamResponseModel PingComplexModel(MedicalExamInputModel inputModel);
-
-        [OperationContract]
-        void VoidMethod(out string s);
-
-        [OperationContract]
-        Task<int> AsyncMethod();
-
-        [OperationContract]
-        int? NullableMethod(bool? arg);*/
-
-        [OperationContract]
-        void XmlMethod(System.Xml.Linq.XElement xml);
     }
 }
--- sasci/Services/MedicalExamService.cs
+++ sasci/Services/MedicalExamService.cs
@@ -15,36 +15,15 @@ namespace SASCi.Services
             return s;
         }
 
-        /*public MedicalExamResponseModel PingComplexModel(MedicalExamInputModel inputModel)
+        public MedicalExamResponseModel PingComplexModel(MedicalExamInputModel inputModel)
         {
             Console.WriteLine("Input data. IntProperty: {0}, StringProperty: {1}", inputModel.IntProperty, inputModel.StringProperty);
 
             return new MedicalExamResponseModel
             {
                 FloatProperty = float.MaxValue / 2,
-                StringProperty = inputModel.StringProperty,
-                ListProperty = inputModel.ListProperty
+                StringProperty = inputModel.StringProperty
             };
-        }
-
-        public void VoidMethod(out string s)
-        {
-            s = "Value from server";
-        }
-
-        public Task<int> AsyncMethod()
-        {
-            return Task.Run(() => 42);
-        }
-
-        public int? NullableMethod(bool? arg)
-        {
-            return null;
-        }*/
-
-        public void XmlMethod(XElement xml)
-        {
-            Console.WriteLine(xml.ToString());
         }
     }
 }
