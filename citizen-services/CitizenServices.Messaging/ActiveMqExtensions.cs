368a7b05ebb8e611b87976dda79f464e3a4ba8f3
davipviana <davipviana@gmail.com>
2021-04-06 17:04:40 -0300

    add messaging classlib

---
+++ citizen-services/CitizenServices.Messaging/ActiveMqExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using ActiveMQ.Artemis.Client;
+using ActiveMQ.Artemis.Client.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace CitizenServices.Messaging
+{
+    public static class ActiveMqExtensions
+    {
+        public static IActiveMqBuilder AddTypedConsumer<TMessage, TConsumer>(this IActiveMqBuilder builder, RoutingType routingType)
+            where TConsumer : class, ITypedConsumer<TMessage>
+        {
+            builder.Services.TryAddScoped<TConsumer>();
+            builder.AddConsumer(typeof(TMessage).Name, routingType, HandleMessage<TMessage, TConsumer>);
+            return builder;
+        }
+
+        private static async Task HandleMessage<TMessage, TConsumer>(Message message, IConsumer consumer, IServiceProvider serviceProvider, CancellationToken token)
+            where TConsumer : class, ITypedConsumer<TMessage>
+        {
+            var msg = JsonSerializer.Deserialize<TMessage>(message.GetBody<string>());
+            using var scope = serviceProvider.CreateScope();
+            var typedConsumer = scope.ServiceProvider.GetService<TConsumer>();
+            await typedConsumer.ConsumeAsync(msg, token);
+            await consumer.AcceptAsync(message);
+        }
+
+        public static IActiveMqBuilder AddTypedConsumer<TMessage, TConsumer>(this IActiveMqBuilder builder, RoutingType routingType, string queue)
+            where TConsumer : class, ITypedConsumer<TMessage>
+        {
+            builder.Services.TryAddScoped<TConsumer>();
+            var address = typeof(TMessage).Name;
+            var queueName = $"{address}/{queue}";
+            builder.AddConsumer(address, routingType, queueName, HandleMessage<TMessage, TConsumer>);
+            return builder;
+        }
+    }
+}
---
+++ citizen-services/CitizenServices.Messaging/ITypedConsumer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CitizenServices.Messaging
+{
+    public interface ITypedConsumer<in T>
+    {
+        Task ConsumeAsync(T message, CancellationToken cancellationToken);
+    }
+}
