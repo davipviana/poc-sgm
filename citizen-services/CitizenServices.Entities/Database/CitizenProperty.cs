6837580063e092a2b364705e9d92f32387f238ae
davipviana <davipviana@gmail.com>
2021-04-07 17:50:23 -0300

    add property list page

--- citizen-services/CitizenServices.Entities/Database/CitizenProperty.cs
+++ citizen-services/CitizenServices.Entities/Database/CitizenProperty.cs
@@ -7,6 +7,10 @@ namespace CitizenServices.Entities.Database
         [Key]
         public int CitizenPropertyId { get; set; }
         public double MarketValue { get; set; }
+        public double? TaxValue { get; set; }
+
+        [StringLength(50)]
+        public string CitizenId { get; set; }
 
         public int CitizenPropertyTypeId { get; set; }
         public virtual CitizenPropertyType CitizenPropertyType { get; set; }
---
+++ citizen-services/CitizenServices/Pages/CitizenProperties.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CitizenServices.Entities.Database;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CitizenServices.Pages
+{
+    public class CitizenPropertiesModel : PageModel
+    {
+        private readonly ILogger<CitizenPropertiesModel> _logger;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+        public IList<CitizenProperty> CitizenProperties { get; set; }
+
+        public CitizenPropertiesModel(ILogger<CitizenPropertiesModel> logger, ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            CitizenProperties = await _dbContext.CitizenProperties.Where(cp => cp.CitizenId == userId).ToListAsync();
+        }
+    }
+}
