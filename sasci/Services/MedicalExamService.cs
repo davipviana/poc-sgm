91b2cdd790e348db0358c2a22c359647340a2959
davipviana <davipviana@gmail.com>
2021-04-08 11:05:03 -0300

    change exam result format

--- sasci/Services/MedicalExamService.cs
+++ sasci/Services/MedicalExamService.cs
@@ -13,7 +13,7 @@ namespace SASCi.Services
         {
             Random r = new Random();
 
-            return $"{s} - {r.Next(0, 100)}%";
+            return $"{r.Next(0, 100)}%";
         }
     }
 }
